47216eac9ff0fb55704ff58103c7fc4d5fab9046
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2025-05-09 17:29:49 +0200

    (feat): add to api

--- src/Persberichten/PostType/PostTypeServiceProvider.php
+++ src/Persberichten/PostType/PostTypeServiceProvider.php
@@ -3,6 +3,7 @@
 namespace OWC\Persberichten\PostType;
 
 use OWC\Persberichten\Foundation\ServiceProvider;
+use OWC\Persberichten\Models\Persbericht;
 use WP_Query;
 
 class PostTypeServiceProvider extends ServiceProvider
@@ -18,6 +19,21 @@ class PostTypeServiceProvider extends ServiceProvider
     {
         $this->plugin->loader->addAction('init', $this, 'registerPostTypes');
         $this->plugin->loader->addAction('pre_get_posts', $this, 'orderByPublishedDate');
+        $this->plugin->loader->addFilter('display_post_states', $this, 'addPostStates', 10, 2);
+    }
+
+    public function addPostStates(array $postStates, \WP_Post $post): array
+    {
+        if ($post->post_type !== 'press-item' || $post->post_status !== 'future') {
+            return $postStates;
+        }
+        $itemModel = Persbericht::makeFrom($post);
+        $embargo  = (bool) $itemModel->getMeta('press_mailing_embargo', '', true, '_owc_');
+        if ($embargo) {
+            $postStates[] = __('Embargo', 'persberichten');
+
+        }
+        return $postStates;
     }
 
     /**
